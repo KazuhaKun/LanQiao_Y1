4bce4c58f6d1fdcaf3610b98a12bb010ba0a5f82
KazuhaKun <zou123@outlook.com>
2025-03-05 18:46:18 +0800

    Complete 11

--- 11-PWM/main.c
+++ 11-PWM/main.c
@@ -1,8 +1,8 @@
 // #include <reg52.h>
 #include <STC15F2K60S2.h>
 
-sbit S4 = P3^3;
-sbit S5 = P3^2;
+sbit L1 = P0^0;
+sbit S7 = P3^0;
 
 //System Init
 void Delay(unsigned int t) {
@@ -18,30 +18,11 @@ void OutPutP0(unsigned char channel,unsigned char dat){
 void Disable_Buzzer(){
     OutPutP0(5,0x00);   // Disable Buzzer
 }
-//Nixie Display
-unsigned char NixieCode[] = {
-    0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 
-    0x77, 0x7C, 0x39, 0x5e, 0x79, 0x71,
-    0x00, 0x40};
-void Nixie(unsigned char mode,unsigned char Location,unsigned char num)
-{
-    switch (mode)
-    {
-        case 1:
-        {
-            OutPutP0(6,0x01 << (Location - 1));
-            OutPutP0(7,~NixieCode[num]);
-            Delay(500);
-            break;
-        }
-        case 2:
-        {
-            OutPutP0(6,Location);
-            OutPutP0(7,~NixieCode[num]);
-            break;
-        }
-    }
-}
+
+
+unsigned char State;
+unsigned char Rate;
+unsigned char RateNum[]={0,25,50,100};
 //Timer0
 void InitTimer0()
 {
@@ -49,65 +30,45 @@ void InitTimer0()
     TMOD |= 0x01;
 
     //定时50ms
-    TH0 = (65535 - 50000) / 256;
-    TL0 = (65535 - 50000) % 256;
+    TH0 = (65535 - 100) / 256;
+    TL0 = (65535 - 100) % 256;
 
     ET0 = 1;
     EA = 1;
     TR0 = 1;
 }
 
-unsigned char minSec,Sec,Min;
-unsigned char State;
-
-void Clear_Time()
+void ServiceTimer0() interrupt 1
 {
-    minSec = 0;
-    Sec = 0;
-    Min = 0;
+    static unsigned char Count = 0;
+    // static unsigned char Count1 = 0;
+    TH0 = (65535 - 100) / 256;
+    TL0 = (65535 - 100) % 256;
+    Count++;
+    // if(Count == Rate) State=!State;
+    // else if(Count == 100) {Count=0;State=!State;}
+    // if(Count == Rate) L1 = 1;
+    // else if(Count == 100) {L1 = 0;Count = 0;}
+    if(Count <= Rate) L1 = 0;
+    else L1 = 1;
+    if(Count >= 100) Count = 0;
 }
 
-void TimeDisplay()
-{
-    Nixie(1,1,Min/10);
-    Nixie(1,2,Min%10);
-    Nixie(1,3,17);
-    Nixie(1,4,Sec/10);
-    Nixie(1,5,Sec%10);
-    Nixie(1,6,17);
-    Nixie(1,7,minSec/10);
-    Nixie(1,8,minSec%10);
-}
-
+//Key
 void KeyDetect()
 {
-    if(S4 == 0)
+    static unsigned char i;
+    unsigned char stat;
+    if(S7 == 0)
     {
         Delay(100);
-        if(S4 == 0) 
+        if(S7 == 0) 
         {
-            State = !State;
-            while(S4 ==0) TimeDisplay();
+            i = (i + 1) % 4;
+            Rate = RateNum[i];
+            while(S7 == 0) Delay(10);
         }
-    }
-    if(S5 == 0)
-    {
         Delay(100);
-        if(S5 == 0)
-        {
-            Clear_Time();
-            while(S5 ==0) TimeDisplay();
-        }
-    }
-}
-
-void Time()
-{
-    if(State == 1)
-    {
-        if(minSec == 20){minSec=0;Sec++;}
-        if(Sec == 60){Sec=0;Min++;}
-        if(Min == 99){Min = 0;}
     }
 }
 
@@ -117,22 +78,8 @@ void main()
     InitTimer0();
     Delay(60000);
     OutPutP0(4,0xff);
-    State = 0;
     while(1)
     {
-        TimeDisplay();
         KeyDetect();
     }
-}
-
-void ServiceTimer0() interrupt 1
-{
-    static unsigned char Count = 0;
-    // static unsigned char Count1 = 0;
-    TH0 = (65535 - 50000) / 256;
-    TL0 = (65535 - 50000) % 256;
-    Count++;
-    if(State == 1 && Count) minSec++;
-    // if(Count) KeyDetect();
-    Time();
 }
