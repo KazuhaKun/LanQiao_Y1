b8aaab5d12f55c76247336101a07ac77e1a58e30
KazuhaKun <zou123@outlook.com>
2025-03-06 20:25:24 +0800

    Complete 13

--- 13-串口进阶/main.c
+++ 13-串口进阶/main.c
@@ -18,8 +18,18 @@ void OutPutP0(unsigned char channel,unsigned char dat){
 void Disable_Buzzer(){
     OutPutP0(5,0x00);   // Disable Buzzer
 }
+void Disable_LED(){
+    OutPutP0(4,0xff);
+}
+void InitSystem(){
+    Disable_Buzzer();
+    Disable_LED();
+}
+
 
 unsigned char urdat;
+
+void SendByte(unsigned char dat);
 
 void InitUART()
 {
@@ -35,21 +45,14 @@ void InitUART()
     EA = 1;
 
 }
-
-void SendByte(unsigned char dat)
-{
-    SBUF = dat;
-    while(TI == 0);
-    TI = 1;
-}
+unsigned char command = 0x00;
 
 void ServiceUART() interrupt 4
 {
     if(RI == 1)
     {
         RI = 0;     //人工清零
-        urdat = SBUF;
-        SendByte(urdat + 1);
+        command = SBUF;
     }
     // if(TI == 1)
     // {
@@ -57,16 +60,52 @@ void ServiceUART() interrupt 4
     // }
 }
 
+void SendByte(unsigned char dat)
+{
+    SBUF = dat;
+    while(TI == 0);
+    TI = 0;
+}
+
+void SendString(unsigned char *str)
+{
+    while(*str != '\0'){
+        SendByte(*str++);
+    }
+}
+
+void Working()
+{
+    if(command != 0x00)
+    {
+        switch(command & 0xf0)
+        {
+            case 0xa0:
+            P0 = (P0 & 0xf0) | ~(command & 0x0f);
+            command = 0x00;
+            break;
+
+            case 0xb0:
+            P0 = (P0 & 0x0F) | ~((command & 0x0f) << 4);
+            command = 0x00;
+            break;
+
+            case 0xc0:
+            SendString("The System is Running...\r\n");
+            command = 0x00;
+            break;
+        }
+    }
+}
+
 void main()
 {
-    Disable_Buzzer();
+    InitSystem();
     Delay(60000);
-    OutPutP0(4,0xff);
     InitUART();
-    SendByte(0x5a);
-    SendByte(0xa5);
+    SendString("Welcome to XMF System!\r\n");
     while(1)
     {
-
+        Working();
     }
 }
---
+++ 15-基础技能综合实训案例/main.c
@@ -0,0 +1,10 @@
+// #include <reg52.h>
+#include <STC15F2K60S2.h>
+
+void main()
+{
+    while(1)
+    {
+        
+    }
+}
